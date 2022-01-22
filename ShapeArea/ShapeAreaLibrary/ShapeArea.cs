ab6474babea3ba14fc9bb9b831ff2303b3a1fc5d
JackenQuake <jackenquake@gmail.com>
2022-01-22 21:00:32 +0300

    Project done

---
+++ ShapeArea/ShapeAreaLibrary/ShapeArea.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea {
+    /// <summary>
+    /// Интерфейс для классов геометрических фигур, реализующий вычисление площади.
+    /// </summary>
+    // Новые фигуры можно добавлять реализуя данный интерфейс,
+    // при этом далее можно использовать метод GetArea() не зная, какая фигура была создана ранее.
+    public interface IShapeArea {
+        double GetArea();
+    }
+    /// <summary>
+    /// Класс Окружность, реализующий интерфейс вычисления площади
+    /// </summary>
+    public class Circle : IShapeArea {
+        private double r;
+        public Circle(double _r) {
+            r = _r;
+        }
+        public double GetArea() {
+            return Math.PI * r * r;
+        }
+    }
+    /// <summary>
+    /// Класс Треугольник, реализующий интерфейс вычисления площади и проверку на прямоугольность
+    /// </summary>
+    public class Triangle : IShapeArea {
+        private double a, b, c;
+        public Triangle(double _a, double _b, double _c) {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+        // для треугольника - Формула Герона
+        public double GetArea() {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        // из теоремы косинусов следует, что равенство a*a + b*b = c*c выполняется только для прямоугольных треугольников,
+        // но аккуратно сравненпие дробных чисел делается с точностью дл 10^(-10)
+        public bool IsSquare() {
+            return (Math.Abs(a * a + b * b - c * c) < 1e-10);
+        }
+    }
+}
---
+++ ShapeArea/ShapeAreaTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeArea;
+
+namespace ShapeAreaTest {
+    class Program {
+        static void TestDoubleValue(string comment, double result, double expected) {
+            if (Math.Abs(result - expected) < 1e-10) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("ВЕРНО   ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            } else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("НЕВЕРНО ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.WriteLine($": {comment}, ожидалось {expected}, получено {result}.");
+        }
+        static void TestBooleanValue(string comment, bool result, bool expected) {
+            if (result == expected) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("ВЕРНО   ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            } else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("НЕВЕРНО ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            Console.WriteLine($": {comment}, ожидалось {expected}, получено {result}.");
+        }
+        static void TestCircle() {
+            var circle1 = new ShapeArea.Circle(10);
+            var circle2 = new ShapeArea.Circle(15);
+            TestDoubleValue("Площадь круга радиуса 10", circle1.GetArea(), 100 * Math.PI);
+            TestDoubleValue("Площадь круга радиуса 15", circle2.GetArea(), 225 * Math.PI);
+        }
+        static void TestTriangle() {
+            var triangle1 = new Triangle(1, 2, 3);
+            var triangle2 = new Triangle(3, 4, 5);
+            TestDoubleValue("Площадь треугольника со сторонами 1, 2, 3", triangle1.GetArea(), 0);
+            TestBooleanValue("Проверка на прямоугольность треугольника со сторонами 1, 2, 3", triangle1.IsSquare(), false);
+            TestDoubleValue("Площадь треугольника со сторонами 3, 4, 5", triangle2.GetArea(), 6);
+            TestBooleanValue("Проверка на прямоугольность треугольника со сторонами 3, 4, 5", triangle2.IsSquare(), true);
+        }
+        static void Main(string[] args) {
+            TestCircle();
+            TestTriangle();
+            Console.WriteLine(); Console.Write("Press any key to exit..."); Console.ReadKey(true);
+        }
+    }
+}
